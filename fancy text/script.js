60463f62bd3db4f81e04ec1cb7076ca5f6c046a1
cyinical <77436545+cyinical@users.noreply.github.com>
2021-01-14 09:37:58 +0000

    Delete fancy text directory

--- fancy text/script.js
+++
@@ -1,9 +0,0 @@
-function randomGreeting(){
-	
-	var myArry = ["A bad penny always turns up","A barking dog never bites","A change is as good as a rest","a good beginning makes a good ending"];
-	
-	var i = Math.floor(Math.random()*4);
-	
-	return(myArry[i] + "...");
-	
-}
